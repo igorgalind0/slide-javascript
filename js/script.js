b6cea5bc1abf0493a00b1bfc58b832b062b5824b
igorgalind0 <igorgalindo950@gmail.com>
2025-09-18 15:32:03 -0300

    configurando a movimentação do slide

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-import Slide from '../js/slide.js';
+import Slide from './slide.js';
 
 const slide = new Slide('.slide', '.slide-wrapper');
 slide.init();
+console.log(slide);
 
 slide.changeSlide(3);
+slide.activePrevSlide();
